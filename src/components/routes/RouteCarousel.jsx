44075a38ded4c43590029dbfaa2a052a96a0dd82
Jeppe Krogh <kjej@aarhus.dk>
2023-07-12 14:36:32 +0200

    adding carousel

--- src/components/routes/RouteCarousel.jsx
+++ src/components/routes/RouteCarousel.jsx
@@ -1,14 +1,24 @@
 import { React } from "react";
+import { Carousel } from "react-responsive-carousel";
 import Route from "./Route";
 import { getIdFromApiEndpoint } from "../../util/helper";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 function RouteCarousel({ routes }) {
   return (
-    <>
+    <Carousel
+      showThumbs={false}
+      showStatus={false}
+      autoplay
+      infiniteLoop
+      showArrows
+      swipeable
+      // onChange={(index) => setSelectedExperienceHandler(index)}
+    >
       {routes.map((route) => (
         <Route key={route} id={getIdFromApiEndpoint(route)} />
       ))}
-    </>
+    </Carousel>
   );
 }
 
