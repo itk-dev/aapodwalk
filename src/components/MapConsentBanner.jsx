4e8cbd185d28752662db7c289e01120befcd0226
Sine Jespersen <sinejespersen@gmail.com>
2024-12-04 13:45:06 +0100

    open street map consent added

---
+++ src/components/MapConsentBanner.jsx
@@ -0,0 +1,60 @@
+import { React, useState, useContext } from "react";
+import PermissionContext from "../context/permission-context";
+import { Link } from "react-router-dom";
+
+const MapConsentBanner = () => {
+  const { setOpenStreetMapConsent, openStreetMapConsent } = useContext(PermissionContext);
+
+  function updateConsent(consent) {
+    localStorage.setItem("data-consent", consent);
+    setOpenStreetMapConsent(consent);
+  }
+
+  // If the user already said yes or no, we will not display the consent banner
+  if (openStreetMapConsent === false || openStreetMapConsent === true) return;
+
+  return (
+    <>
+      <div className="fixed left-3 bottom-3 right-3 dark:bg-zinc-600 flex flex-col gap-3 rounded-lg p-3">
+        <div>
+          <h2 className="mb-2 font-bold">Samtykke</h2>
+          <div className="mt-2 flex flex-col">
+            <div className="flex flex-col gap-2 ">
+              <p>
+                Aarhus Kommune ønsker i forbindelse med Podwalk at indhente dit samtykke til, at vi behandler
+                personoplysninger om dig. Podwalk anvender OpenStreetMap som kortløsning og ved brug af denne indsamles
+                brugrens ip. Hvis du vælger ikke at samtykke vil kortet ikke være tilgængelig i Podwalk. Det samtykke,
+                du har givet til behandlingen, kan til enhver tid trækkes tilbage, dog uden at ændrer på lovligheden af
+                den behandling, der allerede måtte være foretaget. Du kan tilbagetrække et afgivet samtykke ved at
+                henvende dig, mundtligt eller skriftligt, til Aarhus Kommune, Kultur- og Borgerservice
+                <a href="mailto:itkdev@mkb.aarhus.dk"> itkdev@mkb.aarhus.dk</a> itkdev@mkb.aarhus.dk og bede om at få samtykket trukket tilbage. Læs mere om, hvordan vi behandler dine
+                personoplysninger.
+              </p>
+              <div className="mt-3">
+                <Link className="underline" to={`/personal-information-policy`}>
+                  Læs mere om, hvordan vi behandler dine personoplysninger.
+                </Link>
+              </div>
+              <button
+                className="p-1 rounded text-zinc-800 bg-zinc-100 float-right my-1"
+                type="button"
+                onClick={() => updateConsent(true)}
+              >
+                Ja
+              </button>
+              <button
+                className="p-1 rounded text-zinc-800 bg-zinc-100 float-right"
+                type="button"
+                onClick={() => updateConsent(false)}
+              >
+                Nej
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MapConsentBanner;
--- src/components/tags/TagsList.jsx
+++ src/components/tags/TagsList.jsx
@@ -1,12 +1,11 @@
 import { React, useEffect, useState } from "react";
 import useFetch from "../../util/useFetch";
 import Tag from "./Tag";
-import Xmark from "../../icons/xmark-solid.svg";
+import MapConsentBanner from "../MapConsentBanner";
 
 function TagsList() {
   const [tags, setTags] = useState(null);
   const { data } = useFetch(`tags`);
-  const [viewAbout, setViewAbout] = useState(false);
 
   useEffect(() => {
     if (data) {
@@ -23,42 +22,7 @@ function TagsList() {
         <Tag key={name} numberOfRoutes={routes.length} name={name} id={id} />
       ))}
 
-      <div className="fixed left-3 bottom-3 right-3 bg-sky-400 dark:bg-sky-900 flex flex-col gap-3 rounded-lg p-3">
-        {!viewAbout && (
-          <div>
-            <h2 className="mb-2 font-bold">Hvordan fungerer podwalk?</h2>
-            <button
-              className="py-1 px-4 rounded text-zinc-800 bg-zinc-100 flex-auto"
-              type="button"
-              onClick={() => setViewAbout(!viewAbout)}
-            >
-              Se vejledning
-            </button>
-          </div>
-        )}
-        {viewAbout && (
-          <div id="description">
-            <button
-              className="p-1 rounded text-zinc-800 bg-zinc-100 float-right ml-1 mb-1"
-              type="button"
-              aria-label="Luk beskrivelsen"
-              onClick={() => setViewAbout(!viewAbout)}
-            >
-              <img src={Xmark} alt="Tilbage" className="h-6 w-6 text-zinc-800" />
-            </button>
-            <div className="flex flex-col gap-2">
-              <p>Vælg først en kategori på listen herover.</p>
-              <p>Vælg derefter den ønskede rute.</p>
-              <p>Gå hen til første del af ruten og der kan du starte afspilningen.</p>
-              <p>
-                Lyt til vejledningen eller brug navigations pilen i bunden af rute visningen til at komme til næste
-                punkt. Husk at være opmærksom på dine opgivelser.
-              </p>
-              <p>God fornøjelse med din podwalk.</p>
-            </div>
-          </div>
-        )}
-      </div>
+      <MapConsentBanner></MapConsentBanner>
     </div>
   );
 }
