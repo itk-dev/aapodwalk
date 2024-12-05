a1cfe5814a51c23d0db77113d3f717fb31267f29
Sine Jespersen <sinejespersen@gmail.com>
2024-12-05 21:28:21 +0100

    3175: revert carousel, will be done later

--- src/components/routes/RouteCarousel.jsx
+++ src/components/routes/RouteCarousel.jsx
@@ -12,11 +12,18 @@ function RouteCarousel({ routes, onCarouselChange, hideMapOverlay, setHideMapOve
     <>
       {!hideMapOverlay && (
         <div className="absolute flex justify-end items-end h-56 left-0 bottom-0 right-0 rounded-lg overflow-hidden w-full">
-          <button type="button" className="h-10 mr-3 mb-5 mt-5 p-2 rounded bg-white drop-shadow">
+          <button aria-label="Afspil" type="button" className="h-10 mr-3 mb-5 mt-5 p-2 rounded bg-white drop-shadow">
             <Link to={selectedRoute && `/route/${selectedRoute.id}`}>
-              Start ruten
               <img src={IconCirclePlay} className="w-6 h-6" alt={`Link to ${selectedRoute.name}`} />
             </Link>
+          </button>
+          <button
+            aria-label="Åben/luk kortvisning"
+            onClick={() => setHideMapOverlay(!hideMapOverlay)}
+            type="button"
+            className="h-10 mr-5 mb-5 mt-5 p-2 rounded bg-white drop-shadow"
+          >
+            <img src={IconMap} className="w-6 h-6" alt="" />
           </button>
           <Carousel
             className="absolute left-0 bottom-0 right-0 m-5 rounded-lg overflow-hidden max-h-96 md:max-w-lg"
@@ -31,9 +38,22 @@ function RouteCarousel({ routes, onCarouselChange, hideMapOverlay, setHideMapOve
             emulateTouch
             onChange={(index) => onCarouselChange(index)}
           >
-            sdf
+            {routes.map((route) => {
+              const id = getIdFromApiEndpoint(route);
+              return <Route key={id} id={id} />;
+            })}
           </Carousel>
         </div>
+      )}
+      {hideMapOverlay && (
+        <button
+          aria-label="Åben/luk kortvisning"
+          onClick={() => setHideMapOverlay(!hideMapOverlay)}
+          type="button"
+          className="h-10 absolute bottom-5 right-5 p-2 rounded text-zinc-100 dark:text-zinc-800 bg-zinc-800 dark:bg-zinc-100 drop-shadow"
+        >
+          <img src={IconMap} className="w-6 h-6" alt="" />
+        </button>
       )}
     </>
   );
