de1d57c81d914a2913452bc4615a21fa23bbaa13
Sine Jespersen <sinejespersen@gmail.com>
2024-12-06 09:17:48 +0100

    3175: remove tagsstuff from frontpage

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,5 +1,4 @@
-import { React, useEffect, useState } from "react";
-import useFetch from "../util/useFetch";
+import { React, useState } from "react";
 import MapConsentBanner from "./MapConsentBanner";
 import LandingPage from "./LandingPage";
 import TagList from "./tags/TagList";
@@ -9,24 +8,11 @@ import RouteList from "./routes/RouteList";
 function FrontPage() {
   const [selectedTag, setSelectedTag] = useState(null);
 
-  const [tags, setTags] = useState(null);
-  const { data } = useFetch(`tags`);
-
-  function selectTag(id) {
-    setSelectedTag(id);
-  }
-
-  useEffect(() => {
-    if (data) {
-      setTags(data["hydra:member"]);
-    }
-  }, [data]);
-
   return (
     <div>
       <LandingPage></LandingPage>
       <SelectedTagContext.Provider value={{ selectedTag, setSelectedTag }}>
-        <TagList selectTag={selectTag} />
+        <TagList/>
         <RouteList></RouteList>
       </SelectedTagContext.Provider>
 
