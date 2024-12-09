09834da52cb2f66e0e0950c0bf25c64204a86d90
Sine Jespersen <sinejespersen@gmail.com>
2024-12-09 20:46:10 +0100

    3175: save tag filter in url

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,15 +1,29 @@
-import { React, useContext } from "react";
+import { React, useContext, useEffect } from "react";
 import SelectedTagContext from "../../context/SelectedTagContext";
+import { useSearchParams } from "react-router-dom";
 
 function Tag({ title, id }) {
   const { setSelectedTag, selectedTag } = useContext(SelectedTagContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams) {
+      setSelectedTag(Number(searchParams.get("tag")));
+    }
+  }, []);
+
+  function selectTag(id) {
+    setSearchParams({ tag: id });
+    setSelectedTag(id);
+  }
 
   return (
     <button
-      className={`block  mr-1 rounded px-2 mt-1 h-6 font-bold bg-zinc-200 text-black ${
+      className={`block mr-1 rounded px-2 mt-1 h-6 font-bold bg-zinc-200 text-black ${
         selectedTag === id ? "bg-emerald-400 dark:bg-emerald-800 text-white" : ""
       }`}
-      onClick={() => setSelectedTag(id)}
+      type="button"
+      onClick={() => selectTag(id)}
     >
       <span className="sr-only">Filtrer listen af ruter efter ruter med tagget </span>
       {title}
