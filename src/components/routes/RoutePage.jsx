447470fa5806cbbadfbdfcb3eb947efbca528e27
Sine Jespersen <sinejespersen@gmail.com>
2024-12-10 11:16:39 +0100

    3175: rewrite routepage to fit design

--- src/components/routes/RoutePage.jsx
+++ src/components/routes/RoutePage.jsx
@@ -1,8 +1,11 @@
 import { React, useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useFetch from "../../util/useFetch";
 import RouteContext from "../../context/RouteContext";
-import PointsList from "../points/PointsList";
+import MapWrapper from "../map/MapWrapper";
+import TagList from "../tags/TagList";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function RoutePage() {
   const { id } = useParams();
@@ -32,7 +35,46 @@ function RoutePage() {
 
   if (selectedRoute === null) return null;
 
-  return <PointsList points={selectedRoute.pointsOfInterest} />;
+  const { title, tags, pointsOfInterest, totalDuration, distance, description } = selectedRoute;
+
+  return (
+    <>
+      <MapWrapper mapData={pointsOfInterest} />
+      <div className="p-5 absolute left-0 top-0 right-0 bottom-0 flex flex-col justify-between">
+        <div>
+          <TagList classes="z-5 relative text-lg" tags={tags} />
+          <h1 className="text-4xl z-5 font-extrabold z-5 relative word-break">{title}</h1>
+        </div>
+        <div className="flex flex-col items-end">
+          <div className="w-3/5 mb-10 text-end">Tryk på kortet for at undersøge ruten</div>
+          <div className="bg-emerald-400 w-full dark:bg-emerald-800 mb-3 rounded-md p-3 flex z-5 relative">
+            <div>
+              <div className="font-bold">Distance</div>
+              <div className="text-xl">{distance}</div>
+            </div>
+            <div className="ml-6">
+              <div className="font-bold">Dele</div>
+              <div className="text-xl">{pointsOfInterest.length} styk</div>
+            </div>
+            <div className="ml-6">
+              <div className="font-bold">Afspilningstid</div>
+              <div className="text-xl">{totalDuration} minutter</div>
+            </div>
+          </div>
+          <div className="relative z-5 dark:bg-zinc-800 w-full flex flex-col rounded-lg p-3 mb-3 bg-emerald-400">
+            {description}
+          </div>
+          <Link
+            className="flex items-center mr-1 rounded relative px-5 mt-1 h-9 text-center w-max font-bold bg-zinc-200 text-black z-5"
+            to={`/route/${id}/points`}
+          >
+            <FontAwesomeIcon icon={faPlayCircle} className="mr-1" />
+            Start ruten
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RoutePage;
