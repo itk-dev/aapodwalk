293cdb387205e8a7d5e843ce166f8ba948140eee
Sine Jespersen <sinejespersen@gmail.com>
2024-12-10 11:38:03 +0100

    3175 change mapwrapper to button, as I guess it is a button?

--- src/components/map/MapWrapper.jsx
+++ src/components/map/MapWrapper.jsx
@@ -14,17 +14,18 @@ function MapWrapper({ mapData }) {
 
   return (
     <>
-      <div
+      <button
+        type="button"
         className={
           focusOnMap
-            ? "map-container absolute left-0 top-0 right-0 z-50 m-0 h-screen"
-            : "map-container absolute left-0 top-0 right-0 z-0 "
+            ? "map-container absolute left-0 top-0 right-0 z-50 h-screen"
+            : "map-container absolute left-0 top-0 right-0 z-0"
         }
         onClick={() => setFocusOnMap(true)}
       >
         {focusOnMap && <Map zoomControl={true} mapData={mapData} />}
         {!focusOnMap && <Map zoomControl={false} additionalClass="opacity-10" mapData={mapData} />}
-      </div>
+      </button>
       {focusOnMap && (
         <button
           className="flex place-content-center items-center dark:bg-emerald-800 dark:text-white text-sm absolute right-3 rounded-full top-3 z-50 w-9 h-9 bg-white"
