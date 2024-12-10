8800fecde46e098bdbb6589efe97eebd1084b02c
Sine Jespersen <sinejespersen@gmail.com>
2024-12-10 11:37:14 +0100

    3175: z-index juggling

--- src/components/routes/RoutePage.jsx
+++ src/components/routes/RoutePage.jsx
@@ -39,15 +39,14 @@ function RoutePage() {
 
   return (
     <>
-      <MapWrapper mapData={pointsOfInterest} />
       <div className="p-5 absolute left-0 top-0 right-0 bottom-0 flex flex-col justify-between">
         <div>
           <TagList classes="z-5 relative text-lg" tags={tags} />
-          <h1 className="text-4xl z-5 font-extrabold z-5 relative word-break">{title}</h1>
+          <h1 className="text-4xl font-extrabold z-50 relative word-break">{title}</h1>
         </div>
         <div className="flex flex-col items-end">
           <div className="w-3/5 mb-10 text-end">Tryk på kortet for at undersøge ruten</div>
-          <div className="bg-emerald-400 w-full dark:bg-emerald-800 mb-3 rounded-md p-3 flex z-5 relative">
+          <div className="bg-emerald-400 w-full dark:bg-emerald-800 mb-3 rounded-md p-3 flex z-50 relative">
             <div>
               <div className="font-bold">Distance</div>
               <div className="text-xl">{distance}</div>
@@ -61,11 +60,11 @@ function RoutePage() {
               <div className="text-xl">{totalDuration} minutter</div>
             </div>
           </div>
-          <div className="relative z-5 dark:bg-zinc-800 w-full flex flex-col rounded-lg p-3 mb-3 bg-emerald-400">
+          <div className="relative z-50 dark:bg-zinc-800 w-full flex flex-col rounded-lg p-3 mb-3 bg-emerald-400">
             {description}
           </div>
           <Link
-            className="flex items-center mr-1 rounded relative px-5 mt-1 h-9 text-center w-max font-bold bg-zinc-200 text-black z-5"
+            className="flex items-center mr-1 rounded relative px-5 mt-1 h-9 text-center w-max font-bold bg-zinc-200 text-black z-50"
             to={`/route/${id}/points`}
           >
             <FontAwesomeIcon icon={faPlayCircle} className="mr-1" />
@@ -73,6 +72,7 @@ function RoutePage() {
           </Link>
         </div>
       </div>
+      <MapWrapper mapData={pointsOfInterest} />
     </>
   );
 }
