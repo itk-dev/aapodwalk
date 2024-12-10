8b4cd349e649d8ac1fb2572264decc1af35dad28
Sine Jespersen <sinejespersen@gmail.com>
2024-12-10 13:03:25 +0100

    3175: use shiny new closebutton

--- src/components/map/MapWrapper.jsx
+++ src/components/map/MapWrapper.jsx
@@ -1,10 +1,8 @@
 import { useState, useContext } from "react";
-import Xmark from "../../icons/xmark-solid.svg?url";
 import Map from "./Map";
 import PermissionContext from "../../context/permission-context";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
 import "./map-wrapper.css";
+import CloseButton from "../CloseButton";
 
 function MapWrapper({ mapData }) {
   const [focusOnMap, setFocusOnMap] = useState(false);
@@ -26,16 +24,7 @@ function MapWrapper({ mapData }) {
         {focusOnMap && <Map zoomControl={true} mapData={mapData} />}
         {!focusOnMap && <Map zoomControl={false} additionalClass="opacity-10" mapData={mapData} />}
       </button>
-      {focusOnMap && (
-        <button
-          className="flex place-content-center items-center dark:bg-emerald-800 dark:text-white text-sm absolute right-3 rounded-full top-3 z-50 w-9 h-9 bg-white"
-          type="button"
-          onClick={() => setFocusOnMap(false)}
-        >
-          <FontAwesomeIcon className="z-50 w-5 h-5" icon={faClose} />
-          <span class="sr-only">Luk kortvisning</span>
-        </button>
-      )}
+      {focusOnMap && <CloseButton closeOverlay={() => setFocusOnMap(false)} label="luk kortvising" />}
     </>
   );
 }
