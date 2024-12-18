e9088371592d455508e7fb0453a188c6b0d7cf5a
Sine Jespersen <sinejespersen@gmail.com>
2024-12-18 11:58:29 +0100

    juggle some z-index

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -5,11 +5,11 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
 function CloseButton({ closeOverlay, label = "luk", additionalClasses = "" }) {
   return (
     <button
-      className={`flex place-content-center items-center text-sm absolute right-3 rounded-full top-3 z-50 w-9 h-9 ${additionalClasses}`}
+      className={`${additionalClasses} flex place-content-center items-center text-sm absolute right-3 rounded-full top-3 w-9 h-9`}
       type="button"
       onClick={() => closeOverlay()}
     >
-      <FontAwesomeIcon className="z-50 w-5 h-5" icon={faClose} />
+      <FontAwesomeIcon className="z-40 w-5 h-5" icon={faClose} />
       <span className="sr-only">{label}</span>
     </button>
   );
