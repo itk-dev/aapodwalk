0f912107ef6d3adbde217b257de1e03b0ae5a6be
Sine Jespersen <sinejespersen@gmail.com>
2024-12-18 12:02:54 +0100

    add label for screen reader

--- src/components/points/OrderComponent.jsx
+++ src/components/points/OrderComponent.jsx
@@ -1,9 +1,14 @@
-import { React } from "react";
+import { React, useContext } from "react";
+import RouteContext from "../../context/RouteContext";
 
 const OrderComponent = ({ order }) => {
+  const { selectedRoute } = useContext(RouteContext);
+
   return (
     <div className="flex place-content-center rounded text-xl w-6 h-6 bg-black justify-center mb-2 items-center bg-white flex dark:bg-emerald-800">
+      <span className="sr-only">Dette punkt er nummer </span>
       {order}
+      <span className="sr-only"> på ruten: {selectedRoute.title}</span>
     </div>
   );
 };
