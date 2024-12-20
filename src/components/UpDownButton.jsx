f84dc4a933f5e519cafcfd697eeec9b31d7899e2
Sine Jespersen <sinejespersen@gmail.com>
2024-12-20 14:27:20 +0100

    remove z-index

--- src/components/UpDownButton.jsx
+++ src/components/UpDownButton.jsx
@@ -5,12 +5,12 @@ import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 function UpDownButton({ toggleOverlay, label, up }) {
   return (
     <button
-      className="dark:bg-emerald-800 dark:text-white bg-white z-50 flex place-content-center items-center text-sm absolute right-14 rounded-full top-3 w-9 h-9"
+      className="dark:bg-emerald-800 dark:text-white bg-white flex place-content-center items-center text-sm absolute right-14 rounded-full top-3 w-9 h-9"
       type="button"
       onClick={() => toggleOverlay()}
     >
-      {!up && <FontAwesomeIcon className="z-40 w-5 h-5" icon={faAngleDown} />}
-      {up && <FontAwesomeIcon className="z-40 w-5 h-5" icon={faAngleUp} />}
+      {!up && <FontAwesomeIcon className="w-5 h-5" icon={faAngleDown} />}
+      {up && <FontAwesomeIcon className="w-5 h-5" icon={faAngleUp} />}
       <span className="sr-only">{label}</span>
     </button>
   );
